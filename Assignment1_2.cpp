87175fea959f4c5f5ed49e128b762e0443b6036e
Chetan-80097 <chetanarote5@gmail.com>
2023-09-23 19:32:33 +0530

    Assignment1_2.cpp updated

--- Assignment1_2.cpp
+++ Assignment1_2.cpp
@@ -8,34 +8,42 @@ bool isLeapYear();*/
 #include <iostream>
 
 using namespace std;
-struct Date{
+struct Date
+{
  
      int Day;
      int Month;
      int Year;
 
 
-void initDate()
-{
+  void initDate()
+   {
     Day=0;
     Month=0;
     Year=0;
-}
+   }
 
-void acceptDateFromConsole()
-{ 
+  void acceptDateFromConsole()
+   { 
     cout<<"Enter the Day"<<endl;
     cin >>Day;
     cout<<"Enter the Month"<<endl;
     cin>>Month;
     cout<<"Enter the Year"<<endl;
     cin>>Year;
-}
+   }
 
-void printDateOnConsole()
-{
+  void printDateOnConsole()
+   {
     cout<<Day<<"-"<<Month<<"-"<<Year<<endl;
-}
+   }
+
+  bool isLeapYear()
+   {
+    if ((Year%4 ==0 && Year%100 !=0) || Year%400==0)
+    {return true;}
+    return false;
+   }
 };
 int main()
 {
@@ -44,8 +52,10 @@ int main()
 
     do
     {
-        cout<<"Enter your choice\n"<<"1.Print default Date\n"<<"2.Accept the Date\n"<<"3.Print the Date\n"<<"4. Exit\n";
+        cout<<"=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*"<<endl;
+        cout<<"Enter your choice\n"<<"1.Print default Date\n"<<"2.Accept the Date\n"<<"3.Print the Date\n"<<"4.Check for Leap Year \n"<<"5.Exit"<<endl;
         cin>>choice;
+        cout<<"=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*"<<endl;
         switch(choice)
        { 
         case 1 :
@@ -61,10 +71,24 @@ int main()
                  d.printDateOnConsole();
                  break;
 
-        case 4 : cout<<"\n";
+        case 4 : if(d.isLeapYear())
+                {
+                    cout<<"Year is a Leap Year"<<endl;
+                }
+                else
+                {
+                    cout<<"Year is NOT a Leap Year"<<endl;
+                }
               break;
+
+         case 5 : 
+                  cout<<"Thank you for using our Application"<<endl;
+                  break;     
+
+         default :
+                   cout<<"Entered a wrong choice"<<endl; 
        }      
-    } while (choice != 4);
+    } while (choice != 5);
     
    return 0;
 }
